fd8eeb221fef5f6eb028f9739c35815124ad09e3
LikithaVenna <68274754+LikithaVenna@users.noreply.github.com>
2020-08-14 01:50:20 +0530

    Data Table using Quotes,Parameters and Stored Procedures

---
+++ StudentTableWebForm.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentTableWebForm.aspx.cs" Inherits="StudentDataTableAssignment.StudentTableWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <asp:GridView ID="GridView1" runat="server" BackColor="#CCCCCC" BorderColor="#999999" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="Black" Height="222px" Width="292px">
+            <FooterStyle BackColor="#CCCCCC" />
+            <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" />
+            <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#808080" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#383838" />
+        </asp:GridView>
+        <p>
+            &nbsp;</p>
+        <p>
+            StudRollNo&nbsp;&nbsp;
+            <asp:TextBox ID="txtRollNo" runat="server" style="height: 22px"></asp:TextBox>
+        </p>
+        <p>
+            StudName&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        </p>
+        <p id="txtGrade">
+            StudBranch&nbsp;&nbsp;
+            <asp:TextBox ID="txtBranch" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            StudGrade&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtGrade" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" BackColor="Red" BorderColor="#FF9900" ForeColor="White"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="BtnInsertWithPara" runat="server" OnClick="BtnInsertWithPara_Click" Text="InsertWithParameters" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnInsertWithSp" runat="server" OnClick="btnInsertWithSp_Click" Text="InsertWithSp" />
+        </p>
+        <p>
+            <asp:Button ID="btnUpdateWithQuotes" runat="server" OnClick="btnUpdateWithQuotes_Click" Text="UpdateWithQuotes" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnUpdateWithSp" runat="server" OnClick="btnUpdateWithSp_Click" Text="UpdateWithSp" />
+        </p>
+        <p>
+            <asp:Button ID="btnSearchWithQuotes" runat="server" OnClick="btnSearchWithQuotes_Click" Text="SearchWithQuotes" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnSearchWithPara" runat="server" OnClick="btnSearchWithPara_Click" Text="SearchWithParameters" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+    
+</body>
+</html>
---
+++ StudentTableWebForm.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudentDataTableAssignment
+{
+    public partial class StudentTableWebForm : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-DNNPA9S;Initial Catalog=JulyDotNetBatch2020" +
+        ";Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+        DataTable dt;
+        public void ShowGrid()
+        {
+            conn.Open();
+            cmd = new SqlCommand("select * from StudentTable", conn);
+            dr = cmd.ExecuteReader();
+            dt = new DataTable();
+            dt.Load(dr);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            dr.Close();
+            conn.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowGrid();
+            }
+        }
+
+        protected void BtnInsertWithPara_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("Insert into StudentTable(StudentRollNo," +
+                "StudentName,StudentBranch,StudentGrade)values(@studroll,@studname,@studbranch,@studgrade)", conn);
+            cmd.Parameters.AddWithValue("@studroll", 95);
+            cmd.Parameters.AddWithValue("@studname", "Jai");
+            cmd.Parameters.AddWithValue("@studbranch", "IT");
+            cmd.Parameters.AddWithValue("@studgrade", 9.95);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();         
+        }
+
+        protected void btnInsertWithSp_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("sp_insert", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@studroll", txtRollNo.Text.ToString());
+            cmd.Parameters.AddWithValue("@studname", txtName.Text.ToString());
+            cmd.Parameters.AddWithValue("@studbranch", txtBranch.Text.ToString());
+            cmd.Parameters.AddWithValue("@studgrade", txtGrade.Text.ToString());
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();
+        }
+
+        protected void btnUpdateWithQuotes_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("Update StudentTable set " +
+                "StudentName='"+txtName.Text+"',StudentBranch='"+txtBranch.Text+"'," +
+                "StudentGrade="+txtGrade.Text+" where StudentRollNo="+txtRollNo.Text+"",conn);
+            cmd.Parameters.Add("StudentRollNo", SqlDbType.Int).
+                Value = Convert.ToInt32(txtRollNo.Text);
+            cmd.Parameters.Add("StudentName", SqlDbType.VarChar,20).
+                Value = txtName.Text;
+            cmd.Parameters.Add("StudentBranch", SqlDbType.VarChar,20).
+                Value = txtBranch.Text;
+            cmd.Parameters.Add("StudentGrade", SqlDbType.Float).
+                Value = Convert.ToSingle(txtGrade.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();
+
+        }
+
+        protected void btnUpdateWithSp_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("sp_update", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@studroll", SqlDbType.Int).
+               Value = Convert.ToInt32(txtRollNo.Text);
+            cmd.Parameters.Add("@studname", SqlDbType.VarChar,20).
+                Value = txtName.Text;
+            cmd.Parameters.Add("@studbranch", SqlDbType.VarChar,20).
+                Value = txtBranch.Text;
+            cmd.Parameters.Add("@studgrade", SqlDbType.Float).
+                Value = Convert.ToSingle(txtGrade.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();
+        }
+
+        protected void btnDeleteWithQuotes_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void btnSearchWithQuotes_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("Select StudentName,StudentBranch,StudentGrade from " +
+                "StudentTable where StudentRollNo=" + txtRollNo.Text + "", conn);
+            cmd.Parameters.Add("StudentRollNo", SqlDbType.Int).
+                Value = Convert.ToInt32(txtRollNo.Text);
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                txtName.Text = dr[0].ToString();
+                txtBranch.Text = dr[1].ToString();
+                txtGrade.Text = dr[2].ToString();
+            }
+            else
+            {
+                Label1.Text = "Please enter a valid Student Roll No";
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+        protected void btnSearchWithPara_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("select StudentName,StudentBranch,StudentGrade from StudentTable where StudentRollNo=@studroll", conn);
+            cmd.Parameters.Add("@studroll", SqlDbType.Int).
+                Value = Convert.ToInt32(txtRollNo.Text);
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                txtName.Text = dr[0].ToString();
+                txtBranch.Text = dr[1].ToString();
+                txtGrade.Text = dr[2].ToString();
+            }
+            else
+            {
+                Label1.Text = "Please enter a valid Student Roll No";
+            }
+            dr.Close();
+            conn.Close();
+        }
+    }
+
+}
