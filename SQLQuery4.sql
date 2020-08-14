711ad5ce2d16b8d119729064d52bc159d775a1ad
LikithaVenna <68274754+LikithaVenna@users.noreply.github.com>
2020-08-14 09:50:28 +0530

    Create Stored Procedure to insert values in StudentTable

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -4,6 +4,7 @@ create proc sp_insert(
 @studbranch varchar(20),
 @studgrade varchar(20)
 )
+
 as
 begin
 insert into StudentTable(StudentRollNo,StudentName,StudentBranch,StudentGrade) values(@studroll,@studname,@studbranch,@studgrade)
